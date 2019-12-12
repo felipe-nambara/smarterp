44f2525894be3da5a072ce82f4cad6be8c970e28
Matheus Bona <bona@ecloudc.com.br>
2019-12-12 14:47:46 -0300

    creating oic controller - removing ids bona

--- controllers/oic.js
+++ controllers/oic.js
@@ -1,20 +1,20 @@
 "use strict";
 
 module.exports = app => {
-  app.get("/users", (req, res) => {
+  app.get("/transactions", (req, res) => {
     res.status(200).send({ message: "listing users" });
   });
 
-  app.post("/users", (req, res) => {
+  app.post("/transaction", (req, res) => {
     res.status(201).send({ message: "creating a user" });
   });
 
-  app.put("/users/:id", (req, res) => {
+  app.put("/transsaction/:id", (req, res) => {
     const id = req.params.id;
     res.status(200).send({ message: `update user ${id}` });
   });
 
-  app.delete("/users/:id", (req, res) => {
+  app.delete("/transaction/:id", (req, res) => {
     const id = req.params.id;
     res.status(422).send({ message: `delete user ${id}` });
   });
