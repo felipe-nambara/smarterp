69f74564988c78f868b11ff5fcea1e51f1d93f5e
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-02-26 14:59:25 -0300

    Update delete_from_otc_cascate.sql

--- oic_db/queries/various/delete_from_otc_cascate.sql
+++ oic_db/queries/various/delete_from_otc_cascate.sql
@@ -1,7 +1,7 @@
-delete from invoice_items where id_invoice in ( select id from invoice where order_to_cash_id in (select id from order_to_cash where operation = 'franchise_conciliator'  ) ) ;
-delete from invoice where order_to_cash_id in (select id from order_to_cash where operation = 'franchise_conciliator' )  ;
-delete from payable where receivable_id in ( select id from receivable where order_to_cash_id in (select id from order_to_cash where operation = 'franchise_conciliator' ) ) ;
-delete from supplier where identification_financial_responsible in ( select supplier_identification from payable where receivable_id in ( select id from receivable where order_to_cash_id in (select id from order_to_cash where operation = 'franchise_conciliator'  ) ) ) ;
-delete from receivable where order_to_cash_id in (select id from order_to_cash where operation = 'franchise_conciliator' )  ;
-delete from invoice_customer where order_to_cash_id in (select id from order_to_cash where operation = 'franchise_conciliator' )  ;
-delete from order_to_cash where operation = 'franchise_conciliator' ;
+delete from invoice_items where id_invoice in ( select id from invoice where order_to_cash_id in (select id from order_to_cash where operation = 'smartfin_fee'  ) ) ;
+delete from invoice where order_to_cash_id in (select id from order_to_cash where operation = 'smartfin_fee' )  ;
+delete from payable where receivable_id in ( select id from receivable where order_to_cash_id in (select id from order_to_cash where operation = 'smartfin_fee' ) ) ;
+delete from supplier where identification_financial_responsible in ( select supplier_identification from payable where receivable_id in ( select id from receivable where order_to_cash_id in (select id from order_to_cash where operation = 'smartfin_fee'  ) ) ) ;
+delete from receivable where order_to_cash_id in (select id from order_to_cash where operation = 'smartfin_fee' )  ;
+delete from invoice_customer where order_to_cash_id in (select id from order_to_cash where operation = 'smartfin_fee' )  ;
+delete from order_to_cash where operation = 'smartfin_fee' ;
