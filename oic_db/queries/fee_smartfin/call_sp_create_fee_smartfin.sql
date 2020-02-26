ef9880f5f77709629a761855f29796e851bfe187
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-02-26 15:22:35 -0300

    Update call_sp_create_fee_smartfin.sql

--- oic_db/queries/fee_smartfin/call_sp_create_fee_smartfin.sql
+++ oic_db/queries/fee_smartfin/call_sp_create_fee_smartfin.sql
@@ -1,2 +1,2 @@
-call sp_create_fee_smartfin('Brazil','2020-01-01','2020-02-26');
+call sp_create_fee_smartfin('Brazil',date_add(current_date(), interval -60 day),current_date());
 -- update payable set oic_smartfin_status_transaction = 'waiting_to_be_process', fee_smartfin_otc_id = null
