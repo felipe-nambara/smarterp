80b4b545dd87f02aac3b16b1d95bb4485b472658
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-03-05 10:34:48 -0300

    Create qry_result_error_otc_v2.sql
    Query para analise de integração otc

---
+++ oic_db/queries/various/qry_result_error_otc_v2.sql
@@ -0,0 +1,80 @@
+select 
+	 otc.unity_identification
+    ,otc.erp_business_unit
+    ,otc.erp_legal_entity
+    ,otc.erp_subsidiary
+    ,otc.acronym
+	,otc.erp_invoice_customer_status_transaction
+    ,otc.erp_receivable_status_transaction
+    ,otc.erp_invoice_status_transaction
+    ,count(1)
+from order_to_cash otc 
+
+inner join receivable rec
+on rec.order_to_cash_id = otc.id
+
+where otc.id in ( select id from ctrl_regs2 )
+
+group by otc.unity_identification
+		,otc.erp_business_unit
+		,otc.erp_legal_entity
+		,otc.erp_subsidiary
+		,otc.acronym
+		,otc.erp_invoice_customer_status_transaction
+		,otc.erp_receivable_status_transaction
+		,otc.erp_invoice_status_transaction;
+
+-- select oftv.* from organization_from_to_version oftv where oftv.acronym in ('RJCCOP4','DFCSUD1','SPISCS2');
+-- select otc.* from order_to_cash otc where otc.unity_identification = 360 and otc.erp_receivable_status_transaction = 'error_trying_to_create_at_erp';
+-- select otc.* from order_to_cash otc where otc.unity_identification = 236 and otc.erp_invoice_status_transaction = 'error_trying_to_create_at_erp';
+
+/*
+select distinct
+	otc.country
+    ,otc.unity_identification
+    ,otc.erp_business_unit
+    ,otc.erp_legal_entity
+    ,otc.erp_subsidiary
+    ,otc.acronym
+    ,otc.origin_system
+    ,otc.operation
+    ,otc.minifactu_id
+    ,otc.front_id
+    ,rec.transaction_type
+    ,rec.net_value
+    ,rec.billing_date
+    ,rec.credit_date
+    ,inv.*
+    ,iit.*
+from order_to_cash otc 
+
+inner join invoice_customer ivc
+on ivc.order_to_cash_id = otc.id
+
+inner join receivable rec
+on rec.order_to_cash_id = otc.id
+
+inner join invoice inv
+on inv.order_to_cash_id = otc.id
+
+inner join invoice_items iit
+on iit.id_invoice = inv.id
+
+where otc.minifactu_id in ( 1617118, 1617119 ) ;
+-- and otc.erp_invoice_customer_status_transaction = 'created_at_erp';
+-- and otc.erp_receivable_status_transaction = 'created_at_erp' ;
+-- and otc.erp_invoice_status_transaction = 'created_at_erp';
+*/
+
+/*
+update order_to_cash otc 
+set otc.erp_invoice_customer_status_transaction = 'waiting_to_be_process' 
+where otc.acronym in ('SPISCS2','RJCCOP4','DFCSUD1') 
+and otc.erp_invoice_customer_status_transaction = 'being_processed' ;
+
+update order_to_cash otc 
+set otc.erp_receivable_status_transaction = 'clustered_receivable_created' 
+where otc.acronym in ('SPISCS2','RJCCOP4','DFCSUD1') 
+and otc.erp_receivable_status_transaction = 'being_processed' ;
+
+*/
