f142b4dd6901fc1d2bd0b70990b7fcf6c8a8c021
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-03-05 10:41:17 -0300

    Update select_invoice_customer_join_order_to_cash_otc.sql
    Correção da query para integração de cliente para evitar travamento do fluxo

--- oic_db/queries/otc_invoice_customer/select_invoice_customer_join_order_to_cash_otc.sql
+++ oic_db/queries/otc_invoice_customer/select_invoice_customer_join_order_to_cash_otc.sql
@@ -23,8 +23,7 @@ inner join receivable rec
 on otc.id = rec.order_to_cash_id
 
 inner join invoice_customer_comparation ivcc 
-on ivc.erp_customer_id = ivcc.erp_customer_id 
-and ivc.identification_financial_responsible = ivcc.identification_financial_responsible
+on ivc.identification_financial_responsible = ivcc.identification_financial_responsible
 
 where otc.country = 'Brazil' -- Integração em paralelo por operação do país
 and otc.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
@@ -34,8 +33,7 @@ and otc.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Fil
 and otc.to_generate_customer = 'yes'
 and rec.transaction_type = 'credit_card_recurring' /*'credit_card_recurring', 'debit_card_recurring', 'debit_account_recurring', 'credit_card_tef', 'debit_card_tef', 'credit_card_pos', 'debit_card_pos', 'cash', 'boleto', 'bank_transfer', 'online_credit_card', 'online_debit_card'*/
 and (
-	   ivc.erp_customer_id <> ivcc.erp_customer_id
-	or ivc.full_name <> ivcc.full_name
+	   ivc.full_name <> ivcc.full_name
 	or ivc.type_person <> ivcc.type_person
 	or ivc.nationality_code <> ivcc.nationality_code
 	or ivc.state <> ivcc.state
@@ -87,4 +85,5 @@ and otc.operation = 'person_plan' -- Integração em paralelo por operação (pl
 and otc.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
 and otc.to_generate_customer = 'yes'
 and rec.transaction_type = 'credit_card_recurring' /*'credit_card_recurring', 'debit_card_recurring', 'debit_account_recurring', 'credit_card_tef', 'debit_card_tef', 'credit_card_pos', 'debit_card_pos', 'cash', 'boleto', 'bank_transfer', 'online_credit_card', 'online_debit_card'*/
+and not exists ( select 1 from invoice_customer_comparation ivcc where ivc.identification_financial_responsible = ivcc.identification_financial_responsible )
 and ivc.erp_customer_id is null;
