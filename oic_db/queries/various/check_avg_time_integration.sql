17c9a13ae32ab1b032eed7d82e3f14dab994ba05
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-03-05 10:39:55 -0300

    Update check_avg_time_integration.sql

--- oic_db/queries/various/check_avg_time_integration.sql
+++ oic_db/queries/various/check_avg_time_integration.sql
@@ -10,4 +10,12 @@ from order_to_cash otc
 
 group by otc.country
 		,otc.origin_system
-		,otc.operation
+		,otc.operation;
+        
+select 
+	 timestampdiff(minute,otc.erp_invoice_customer_send_to_erp_at,current_timestamp())
+	 ,otc.*
+from order_to_cash otc
+
+where otc.erp_invoice_customer_status_transaction = 'being_processed'
+and timestampdiff(minute,otc.erp_invoice_customer_send_to_erp_at,current_timestamp()) > 1;
