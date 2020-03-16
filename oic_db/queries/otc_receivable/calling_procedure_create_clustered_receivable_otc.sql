e9814e4f0d55b07666dbda001baac7e6401bd202
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-03-16 20:14:17 -0300

    Update calling_procedure_create_clustered_receivable_otc.sql

--- oic_db/queries/otc_receivable/calling_procedure_create_clustered_receivable_otc.sql
+++ oic_db/queries/otc_receivable/calling_procedure_create_clustered_receivable_otc.sql
@@ -1,7 +1,9 @@
 set sql_mode = traditional;
 call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','credit_card_recurring',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_card_recurring',1500);
 call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_account_recurring',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','credit_card_tef',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_card_tef',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','credit_card_pos',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_card_pos',1500);
 call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','boleto',1500);
-call sp_create_clustered_receivable('Brazil','biosystem','person_plan','credit_card_recurring',1500);
-call sp_create_clustered_receivable('Brazil','biosystem','person_plan','debit_account_recurring',1500);
-call sp_create_clustered_receivable('Brazil','biosystem','person_plan','boleto',1500);
